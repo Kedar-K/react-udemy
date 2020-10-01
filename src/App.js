93e64cf62cdd449764d9e6fbfaa92932a5063417
Kedar.K <kedarkodgire.kk@gmail.com>
2020-10-02 00:00:45 +0530

    Day 3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     persons: [
       {name: "kk", age: "22"}
     ],
-    something: "this is something"
+    something: "this is something",
+    showPersons: false
   }
 
   switchNameHandeller = (newName) =>{
@@ -29,24 +30,35 @@ class App extends Component {
     )
   }
 
+  togglePersonsHandeller = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
   render() {
     const style = {
       backgroundColor: 'yellow',
       border: 'none',
       padding:'20px'
     }
+
+    let persons = null;
+    if(this.state.showPersons){
+      persons =(
+        <div >
+          {this.state.persons.map(person =>{
+            return <Person name={person.name} age={person.age} changed={this.nameChangedhandler} />
+          })}
+        </div>
+      )
+    }
+
     return (
       <div className="App">
         <h1>Hi react app</h1>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}
-          click={() => this.switchNameHandeller("Kedar!!!")}
-          changed={this.nameChangedhandler}>
-          Hobbies:asasdfs
-          </Person>
-        <button style={style  }
-         onClick={this.switchNameHandeller.bind(this, "kedar")}>switch name</button>
+        {persons}
+        <button style={style}
+         onClick={this.togglePersonsHandeller}>switch name</button>
         <p>{this.state.something}</p>
       </div>
     );
